2ba2dd998bbf7e07e18a64dd97a4a371c05df6e8
sayedmahmoud223 <sbendary977@gmail.com>
2025-06-04 04:30:03 +0300

    change file format for teacher image

--- src/controllers/Auth/authRouting.js
+++ src/controllers/Auth/authRouting.js
@@ -4,7 +4,7 @@ import { asyncHandler } from "../../utilis/ErrorHandling.js"
 import { fileType, fileUpload } from "../../utilis/multer.js"
 const router = express.Router()
 
-router.post("/register", fileUpload([fileType.pdf,fileType.image]).single("idCardImage"), asyncHandler(authController.register))
+router.post("/register", fileUpload(fileType.image).single("idCardImage"), asyncHandler(authController.register))
 // router.post("/register/teacher", fileUpload(fileType.pdf).single("idCardImage"), asyncHandler(authController.registerAsTeacher))
 router.post("/login", asyncHandler(authController.login))
 
